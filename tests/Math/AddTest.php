ac1b63c326b33fd5b27e9cdcaf2c3f0aaa52f9fd
Damien Belingheri <belingheridamien@gmail.com>
2022-04-28 00:41:26 +0200

    test

--- tests/Math/AddTest.php
+++ tests/Math/AddTest.php
@@ -2,12 +2,15 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Test\AbstractFixerTestCase;
+
 /*
  * This file is part of the SolidWorx Lodash-PHP project.
  *
  * @author     Pierre du Plessis <open-source@solidworx.co>
  * @copyright  Copyright (c) 2017
  */
+
 
 use PHPUnit\Framework\TestCase;
 use function _\add;
@@ -16,6 +19,6 @@ class AddTest extends TestCase
 {
     public function testAdd()
     {
-        $this->assertSame(10, add(6, 4));
+        $this->assertEquals(10, add(6, 4));
     }
 }
